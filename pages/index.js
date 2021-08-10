06032f4145dd3f3b271b4736a050dd666344c2a8
younicornsxyz <88727020+younicornsxyz@users.noreply.github.com>
2021-08-10 23:07:08 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -274,13 +274,13 @@ export default function Home() {
             </p>
           </div>
           <div className="link-list">
-            <a className="btn rounded-pill px-3 mb-2 mb-lg-0 hover" href="https://twitter.com/younicorns_nfts" target="_blank" rel="noopener noreferrer">
+            <a className="btn rounded-pill px-3 mb-2 mb-lg-0 hover" href="https://twitter.com/younicorns_nfts">
               <span className="d-flex align-items-center">
                 <i className="bi-twitter me-2"></i>
                 <span className="small">Twitter</span>
               </span>
             </a>
-            <a className="btn rounded-pill px-3 mb-2 mb-lg-0 hover" href="https://discord.gg/d3hYvT3VpV" target="_blank" rel="noopener noreferrer">
+            <a className="btn rounded-pill px-3 mb-2 mb-lg-0 hover" href="https://discord.gg/d3hYvT3VpV">
               <span className="d-flex align-items-center">
                 <i className="bi-discord me-2"></i>
                 <span className="small">Discord</span>
@@ -288,7 +288,7 @@ export default function Home() {
             </a>
           </div>
         </div>
-      </footer>
-    </div>
+      </footer >
+    </div >
   )
 }
